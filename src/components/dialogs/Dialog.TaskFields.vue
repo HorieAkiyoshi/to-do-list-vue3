2e14f0f97b6905cfca8b46ad95aa68583aab0b59
Horie Akiyoshi <h.a.horie@gmail.com>
2025-09-13 19:11:47 +0900

    first commit

---
+++ src/components/dialogs/Dialog.TaskFields.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <v-dialog
+      v-model="taskStore.showDialogTaskFields"
+      persistent
+      min-width="400"
+      max-width="600"
+    >
+
+      <!-- <template v-slot:default="{ props }">
+        <v-btn
+          color="primary"
+          v-bind=props
+        >
+          Open Dialog
+        </v-btn>
+      </template> -->
+
+      <v-card >
+        <v-card-title class="text-h5">
+          Edit task
+        </v-card-title>
+
+        <v-card-text>
+          <v-text-field
+            label="title"
+            v-model="props.task.title"
+          ></v-text-field>
+          <v-text-field
+            label="description"
+            v-model="props.task.description"
+          ></v-text-field>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn
+            color="primary-darken-1"
+            variant="elevated"
+            @click="taskStore.updateTask()"
+          >Ok</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+import  { useTaskStore } from '@/stores/task'
+
+const taskStore = useTaskStore();
+
+const props = defineProps({
+  task: Object
+})
+
+</script>
---
+++ src/components/dialogs/DialogDelete.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-row justify="center">
+    <v-dialog
+      v-model="taskStore.showDialogDelete"
+      persistent
+      min-width="400"
+      max-width="600"
+    >
+
+      <!-- <template v-slot:default="{ props }">
+        <v-btn
+          color="primary"
+          v-bind=props
+        >
+          Open Dialog
+        </v-btn>
+      </template> -->
+
+      <v-card >
+        <v-card-title class="text-h5">
+          Are you sure you want delete this task?
+        </v-card-title>
+
+        <v-card-text>
+          This action can`t be reverted...
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+
+          <v-btn
+            color="red-darken-1"
+            variant="elevated"
+            @click="taskStore.toggleDelete"
+          >No</v-btn>
+
+          <v-btn
+            color="red-darken-1"
+            variant="outlined"
+            @click="taskStore.deleteTask"
+          >Yes</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script setup>
+import  { useTaskStore } from '@/stores/task'
+
+const taskStore = useTaskStore();
+
+</script>
